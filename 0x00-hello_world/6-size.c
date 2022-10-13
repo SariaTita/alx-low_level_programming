297d190279e99d41ca5dac5d01e390a5da3df097
SariaTita <sariaabbas12345@gmail.com>
2022-10-13 15:31:36 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
 * main - Entry point
-* Description: 'Write a C program that prints the size of various types on the computer.'
+* Description: 'Write a C program that prints the size of various types.'
 * Return: Always 0 (Success)
 */
 int main(void)
