54dc0bf9ae457ebe3b029a7fade2bf5a62de8eb0
SariaTita <sariaabbas12345@gmail.com>
2022-10-13 13:50:09 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#includee<stdio.h>
+/**
+ * main - Printing
+ *
+ * Return Always 0 (Succssfull)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return(0);
+}
