a0aed7ba9cf45db9fb605860cd289c5fba5e6111
SariaTita <sariaabbas12345@gmail.com>
2022-10-13 13:26:27 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include<stdio.h>
-
 /**
- *main- Printing
+ * main - Printing
  *
  * Return : Always 0 (successful)
  */
-int main(void)
+int main()
 {
-
-  
-      puts("\"Programming is like building a multilingual puzzle");
-      return(0);
-
+  puts ("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
