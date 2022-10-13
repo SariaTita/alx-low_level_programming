b62fe817c6cb8c11149c102283dda75ea40c7050
SariaTita <sariaabbas12345@gmail.com>
2022-10-13 12:40:07 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
-int main(void){
+/**
+ *main- Printing
+ *
+ * Return : Always 0 (successful)
+ */
+int main(void)
+{
+  
   puts("\"Programming is like building a multilingual puzzle");
   return(0);
 }
