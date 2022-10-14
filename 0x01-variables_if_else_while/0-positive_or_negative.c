aa632848a387c46203f56ef710b7a19c0ccc042e
SariaTita <sariaabbas12345@gmail.com>
2022-10-14 11:28:05 -0700

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,13 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if(n > 0)
+if (n > 0)
 {
 printf("is positive");
 }
 else
 {
-if(n == 0)
+if (n == 0)
 {
 printf("is zero");
 }
