26776297ab2a34ab6b711474244ec97cc7e8f734
SariaTita <sariaabbas12345@gmail.com>
2022-10-14 12:52:49 -0700

    1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,17 +13,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("last digit of %d is %o and is greater than 5\n", n, n);
+printf("last digit of %d is %ld and is greater than 5\n", n, n);
 }
 else
 {
 if (n ==  0)
 {
-  printf("last digit of %d is %o and is 0\n", n, n);
+printf("last digit of %d is %ld and is 0\n", n, n);
 }
 else
 {
-  printf("last digit of %d is %d and is less than 6 and not 0\n", n, n);
+printf("last digit of %d is %ld and is less than 6 and not 0\n", n, n);
 }
 }
 return (0);
