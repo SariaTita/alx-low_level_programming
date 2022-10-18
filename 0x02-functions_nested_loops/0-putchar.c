1315788c1caec1836e112801ffdc1ca77f3da09b
SariaTita <sariaabbas12345@gmail.com>
2022-10-18 09:36:26 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,16 @@
+#include <unistd.h>
 #include "main.h"
 /**
 * main - entry point
 * putchar function
 * return : always 0
 */
+int _putchar(char c)
+{
+return (write(1, &c, 1));  
+}
 int main(void)
 {
 _putchar(_putchar);
 return (0);
 }
-
