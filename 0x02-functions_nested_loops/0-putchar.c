db359329329e0d87cd481c75d1925cc66c7d2d0f
SariaTita <sariaabbas12345@gmail.com>
2022-10-18 11:45:12 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
-* main -  a program that prints _putchar
+* _putchar - writes the character c to stdout
+* @c: The character to print
 *
-* Return : always 0
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
 */
 int main(void)
 {
