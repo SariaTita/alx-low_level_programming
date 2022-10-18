9be9cc002cb3d3f5c5cc7db3bce608ca3c915d04
SariaTita <sariaabbas12345@gmail.com>
2022-10-18 10:09:48 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* main - entry point
+* for loop
+* return : always 0
+*/
+void print_alphabet(void)
+{
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
