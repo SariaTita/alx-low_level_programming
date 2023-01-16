d47e02eda2f40eacc1741781b39b23958128835b
SariaTita <Sariaabbas12345@gmail.com>
2023-01-16 08:21:49 -0800

    -

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _strlen - Returns the length of a string.
+* @s: string.
+*
+* Return: length.
+*/
+int _strlen(char *s)
+{
+int len = 0;
+while (*s++)
+len++;
+return (len);
+}
